b128f34349301dc53d6412f1f2df3151f4192600
kangwongu <kwg0527@naver.com>
2023-10-04 12:58:32 +0900

    상품등록 local repo -> JPA

--- src/main/java/com/example/productorderservice/product/Product.java
+++ src/main/java/com/example/productorderservice/product/Product.java
@@ -1,13 +1,21 @@
 package com.example.productorderservice.product;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
 class Product {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final String name;
-    private final int price;
-    private final DiscountPolicy discountPolicy;
+    private String name;
+    private int price;
+    private DiscountPolicy discountPolicy;
 
     public Product(final String name, final int price, final DiscountPolicy discountPolicy) {
         Assert.hasText(name, "상품명은 필수입니다.");
@@ -18,11 +26,4 @@ class Product {
         this.discountPolicy = discountPolicy;
     }
 
-    public void assignId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
