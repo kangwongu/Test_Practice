96188b995307c4241e9d264e3476deaa6a1e3a7c
kangwongu <kwg0527@naver.com>
2023-10-04 12:28:14 +0900

    상품등록 SpringBoot -> API 테스트

---
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -0,0 +1,42 @@
+package com.example.productorderservice.product;
+
+import com.example.productorderservice.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+public class ProductApiTest extends ApiTest {
+
+    @Test
+    public void 상품등록() {
+        // given
+        final AddProductRequest request = 상품등록요청_생성();
+
+        // when
+        ExtractableResponse<Response> response = 상품등록요청(request);
+
+        // then
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private static ExtractableResponse<Response> 상품등록요청(AddProductRequest request) {
+        return RestAssured.given().log().all()     // 로그를 남기겠다는 의미
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/products")
+                .then()
+                .log().all().extract();
+    }
+
+    private static AddProductRequest 상품등록요청_생성() {
+        final String name = "상품명";
+        final int price = 1000;
+        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+        return new AddProductRequest(name, price, discountPolicy);
+    }
+}
