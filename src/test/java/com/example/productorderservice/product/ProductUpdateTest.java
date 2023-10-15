3f3dfd3591098c26780466594c0d666d3cffb22e
kangwongu <kwg0527@naver.com>
2023-10-15 23:10:30 +0900

    상품조회 Green 단계 POJO 테스트 using Mockito

--- src/test/java/com/example/productorderservice/product/ProductUpdateTest.java
+++ src/test/java/com/example/productorderservice/product/ProductUpdateTest.java
@@ -3,16 +3,24 @@ package com.example.productorderservice.product;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 public class ProductUpdateTest {
 
     private ProductService productService;
-    private StupProductPort productPort = new StupProductPort();
+//    private StupProductPort productPort = new StupProductPort();
+    private ProductPort productPort;
+
 
     @BeforeEach
     void setUp() {
+        productPort = Mockito.mock(ProductPort.class);
+
         productService = new ProductService(productPort);
     }
 
@@ -22,7 +30,8 @@ public class ProductUpdateTest {
         final Long productId = 1L;
         final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
         final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
-        productPort.getProduct_will_return  = product;
+//        productPort.getProduct_will_return  = product;
+        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
 
         // when
         productService.updateProduct(productId, request);
@@ -32,18 +41,18 @@ public class ProductUpdateTest {
         assertThat(product.getPrice()).isEqualTo(2000);
     }
 
-    private class StupProductPort implements ProductPort {
-
-        public Product getProduct_will_return;
-
-        @Override
-        public void save(Product product) {
-
-        }
-
-        @Override
-        public Product getProduct(long productId) {
-            return getProduct_will_return;
-        }
-    }
+//    private class StupProductPort implements ProductPort {
+//
+//        public Product getProduct_will_return;
+//
+//        @Override
+//        public void save(Product product) {
+//
+//        }
+//
+//        @Override
+//        public Product getProduct(long productId) {
+//            return getProduct_will_return;
+//        }
+//    }
 }
