ea284339419134bd6a4bbc4455a01a28f6a274bc
kangwongu <kwg0527@naver.com>
2023-12-28 20:23:31 +0900

    Initial commit

---
+++ test_cafekiosk/src/main/java/com/testcafekiosk/TestCafekioskApplication.java
@@ -0,0 +1,13 @@
+package com.testcafekiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCafekioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCafekioskApplication.class, args);
+    }
+
+}
